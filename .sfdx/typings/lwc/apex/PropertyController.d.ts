19f74f0a01dae7d7a648c34246ae6b2a874cb640
carmen.chea <carmen.chea@accenture.com>
2022-09-28 15:53:55 +0930

    complete using package

--- .sfdx/typings/lwc/apex/PropertyController.d.ts
+++ .sfdx/typings/lwc/apex/PropertyController.d.ts
@@ -1,3 +1,6 @@
+declare module "@salesforce/apex/PropertyController.getPropertyList" {
+  export default function getPropertyList(param: {searchKey: any, maxPrice: any, minBedrooms: any, minBathrooms: any}): Promise<any>;
+}
 declare module "@salesforce/apex/PropertyController.getPagedPropertyList" {
   export default function getPagedPropertyList(param: {searchKey: any, maxPrice: any, minBedrooms: any, minBathrooms: any, pageSize: any, pageNumber: any}): Promise<any>;
 }
